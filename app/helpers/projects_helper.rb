b31451eead82da8a560869a7f57aa52c60bac276
Partho Ghosh <partho.ghosh24@gmail.com>
2020-08-02 13:40:04 +0530

    Alt tags added. Og title updated

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,9 @@
 module ProjectsHelper
     def image_resolver(project)
         if [1,3].include? project.id # horizontal image
-            image_tag  project.logo_url, class: "horiz-img"
+            image_tag  project.logo_url, class: "horiz-img", alt: project.title
         else
-            image_tag  project.logo_url
+            image_tag  project.logo_url, alt: project.title
         end
     end
 end
