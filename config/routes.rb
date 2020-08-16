c84d690ee4121fbe3e0b37802742fe9b01c8dff2
Partho Ghosh <partho.ghosh24@gmail.com>
2020-08-16 23:48:37 +0530

    Added basecamp page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'portfolios#home'
   get '/blog', to: 'portfolios#blog'
   get '/resume', to: 'portfolios#resume'
+  get '/basecamp', to: 'portfolios#basecamp'
   resources :projects, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
